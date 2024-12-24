fbb96b32d4543a6c12cf05cd17377d4f27d98c95
Jose Diego Cardona Gutierrez <josecardona@sanmateo.edu.co>
2024-12-24 17:56:35 -0500

    Primer commit: Proyecto inicializado

---
+++ src/components/header/jsx.java
@@ -0,0 +1,16 @@
+
+import React from 'react';
+public class jsx {
+const header = () => {
+    return (
+        <header>
+            <h1>Bienvenido a mi aplicación</h1>
+            <nav>
+            <a href="#">Inicio</a> | <a href="/About">Acerca</a>
+            </nav>
+        </header>
+    );
+    };
+    
+    export default header;
+};
